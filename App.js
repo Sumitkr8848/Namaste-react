68cdbf86e51b132703b0e2baeb7ebb349898950c
sumit8848 <codersumit335@gmail.com>
2023-12-21 08:31:35 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,2 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+const heading=(
+    <h1 className="head" tabIndex="5">This is sumit kumar from NHCE bangalore
+    </h1>
+)
+//const heading =React.createElement("h1",{id:"Heading"},"HELLO CODERS!!");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
